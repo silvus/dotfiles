c1a530f0873622219dd18cb7cef7f06476c55789
silvus <silvus@silvus.fr>
2014-05-24 16:24:52 +0200

    No more ubuntu 12.04 support

--- utilities.sh
+++ utilities.sh
@@ -1,35 +1,19 @@
 #!/usr/bin/env bash
 
-OS_VERSION_NAME=$(lsb_release -cs)
-
 sudo apt-get update
-
-# Ubuntu 14.04 specific
-# ----------------------------------------------------------------
-if [ $OS_VERSION_NAME = 'trusty' ]; then
-	sudo apt-get install \
-		dfc `# Disk space` \
-		iftop `# htop for net traffic`
-fi
-
-# Ubuntu 12.04 specific
-# ----------------------------------------------------------------
-if [ $OS_VERSION_NAME = 'precise' ]; then
-	# Add PPA for tmux 1.8
-	sudo apt-get install python-software-properties
-	sudo add-apt-repository ppa:kalakris/tmux
-fi
 
 # Installation
 # ----------------------------------------------------------------
 sudo apt-get install \
-	vim `# Text editor` \
-	htop `# Better "top" command` \
 	curl `# Play with HTTP` \
 	wget `# Downloader` \
 	rsync `# Synchronisation` \
+	htop `# Better top command` \
+	dfc `# Disk space` \
+	iftop `# Traffic htop-like` \
 	build-essential `# Compiling things` \
 	ack-grep `# Search` \
+	vim `# Text editor` \
 	tmux `# Split terminal` \
 	lynx `# Web browser` \
 	newsbeuter `# Rss reader`
