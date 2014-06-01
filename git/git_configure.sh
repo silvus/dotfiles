62ceb13ef41e89cc4b2293b2d8115e2992b748b4
silvus <silvus@silvus.fr>
2014-06-01 17:09:01 +0200

    Add git config

---
+++ git/git_configure.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Configure git
+# -----------------------------------------------------------------------------
+git config --global user.name "silvus"
+# git config --global user.email "silvus@MAIL.com"
+
+git config --global color.ui "auto"
+
+git config --global core.editor "vim"
+git config --global core.autocrlf "input"
+
+git config --global alias.st "status"
+git config --global alias.co "commit -a"
+git config --global alias.lg "log --oneline --graph --decorate"
+git config --global alias.gr "grep -p"
+git config --global alias.aa "add --all"
+git config --global alias.co "commit -a"
+git config --global alias.pusom "push origin master"
+git config --global alias.pulom "pull origin master"
+git config --global alias.dif "diff --stat=160,120"
+
+git config --global web.browser "lynx"
+
+git config --global diff.tool "vimdiff"
+git config --global difftool.prompt false
+
+git config --global github.user "Silvus"
+git config --global credential.helper "cache --timeout=3600"
