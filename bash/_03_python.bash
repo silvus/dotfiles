08053b61d444d41017f7c4d59555eb75c5f24164
silvus <silvus@silvus.fr>
2014-06-05 22:34:46 +0200

    Add autocomplete to pip

--- bash/_03_python.bash
+++ bash/_03_python.bash
@@ -22,6 +22,14 @@ dev() {
 # -----------------------------------------------------------------------------
 export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
 export PIP_REQUIRE_VIRTUALENV=true
+# pip bash completion
+_pip_completion()
+{
+    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   PIP_AUTO_COMPLETE=1 $1 ) )
+}
+complete -o default -F _pip_completion pip
 
 # Virtualenvwrapper
 # -----------------------------------------------------------------------------
