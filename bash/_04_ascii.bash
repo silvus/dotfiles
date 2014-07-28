fd9ba365a4cfc6c586da5350fe927faf5e0d863f
silvus <silvus@silvus.fr>
2014-07-28 19:48:10 +0200

    Cleanup ascii

--- bash/_04_ascii.bash
+++ bash/_04_ascii.bash
@@ -13,32 +13,6 @@ _uptime() {
 	echo "$days days, $hours hours, $minutes minutes, $seconds seconds"
 }
 
-# Get pending updates
-# --------------------------------------------------------------------
-_packageupdate() {
-	# Query pending updates.
-	local updates=$(/usr/lib/update-notifier/apt-check 2>&1)
-
-	if [ $? -ne 0 ]; then
-		echo "Querying pending updates failed"
-		exit 1
-	fi
-
-	# Check for the case where there are no updates.
-	if [ "$updates" = "0;0" ]; then
-		echo "All packages are up-to-date"
-		exit 0
-	fi;
-
-	# Check for pending security updates.
-	local pending_security=$( echo "$updates" | cut -d ";" -f 2)
-	# Check for pending non-security updates.
-	local pending_regular=$( echo "$updates" | cut -d ";" -f 1)
-
-	echo "$pending_security security update(s) - $pending_regular non-security update(s)"
-	exit 0
-}
-
 
 # Print ascii art and some system infos
 # --------------------------------------------------------------------
@@ -56,13 +30,13 @@ asciiart() {
 	local backgreen=$(tput -Txterm setaf 0;tput setab 2)
 	local reset=$(tput -Txterm sgr0)
 
-	echo -e "$backgreen"'      .--.     '"$green" "${USER^} - ${HOST^} - $IP_LOCAL"
-	echo -e "$backgreen"'     |o_o |    '"$green" "$OS $ARCH $KERNEL"
-	echo -e "$backgreen"'     |:_/ |    '
-	echo -e "$backgreen"'    //   \ \   '"$green" "Uptime $UPTIME"
-	echo -e "$backgreen"'   (|     | )  '
-	echo -e "$backgreen"'  / \_   _/ \  '
-	echo -e "$backgreen"'  \___)=(___/  '"$green" "${DATE^}"
+	echo -e "$backgreen"'      .--.     '"$green ${USER^} - ${HOST^} - $IP_LOCAL $reset"
+	echo -e "$backgreen"'     |o_o |    '"$green $OS $ARCH $KERNEL $reset"
+	echo -e "$backgreen"'     |:_/ |    '"$reset"
+	echo -e "$backgreen"'    //   \ \   '"$green Uptime $UPTIME $reset"
+	echo -e "$backgreen"'   (|     | )  '"$reset"
+	echo -e "$backgreen"'  / \_   _/ \  '"$reset"
+	echo -e "$backgreen"'  \___)=(___/  '"$green ${DATE^}"
 	echo -e  "$reset"
 }
 
