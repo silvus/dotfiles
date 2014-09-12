7c78005475fa01dccda8e7f094f2b4ce994a960c
silvus <silvus@silvus.fr>
2014-09-12 16:42:30 +0200

    Improve prompt with git

--- bash/_02_prompt.bash
+++ bash/_02_prompt.bash
@@ -17,71 +17,97 @@ reset=$(tput -Txterm sgr0)
 terminal_width=$(tput cols)
 # terminal_height=$(tput lines)
 
+# TODO : Do something more clean with this
+# https://github.com/taringamberini/vcs-bash-prompt
+#_prompt_git() {
+#    $(git rev-parse --is-inside-git-dir 2>/dev/null ) \
+#        && return 1
+#    $(git rev-parse --is-inside-work-tree 2>/dev/null ) \
+#        || return 1
+#    git status &>/dev/null
+#    branch=$(git symbolic-ref --quiet HEAD 2>/dev/null ) \
+#        || branch=$(git rev-parse --short HEAD 2>/dev/null ) \
+#        || branch='unknown'
+#    branch=${branch##*/}
+#    git diff --quiet --ignore-submodules --cached \
+#        || state=${state}+
+#    git diff-files --quiet --ignore-submodules -- \
+#        || state=${state}!
+#    $(git rev-parse --verify refs/stash &>/dev/null ) \
+#        && state=${state}^
+#    [ -n "$(git ls-files --others --exclude-standard )" ] \
+#        && state=${state}?
+#    printf '[%s]' "${branch:-unknown}${state}"
+#}
 
 # Utility function so we can test for things like .git/.hg without firing up a separate process
 _has_parent_dir() {
-	test -d "$1" && return 0;
+    test -d "$1" && return 0;
 
-	current="."
-	while [ ! "$current" -ef "$current/.." ]; do
-		if [ -d "$current/$1" ]; then
-			return 0;
-		fi
-		current="$current/..";
-	done
+    current="."
+    while [ ! "$current" -ef "$current/.." ]; do
+        if [ -d "$current/$1" ]; then
+            return 0;
+        fi
+        current="$current/..";
+    done
 
-	return 1;
+    return 1;
 }
 
-_vcs_name() {
-	if [[ -d ".svn" ]]; then
-		echo "-[svn]";
-	elif _has_parent_dir ".git"; then
-		_vcs_prompt_git
-	elif _has_parent_dir ".hg"; then
-		echo "-[hg $(hg branch)]"
-	fi
+_end_prompt() {
+    if [[ -d ".svn" ]]; then
+        echo "${yellow}-[svn]${reset}";
+    elif _has_parent_dir ".git"; then
+        _vcs_prompt_git
+    elif _has_parent_dir ".hg"; then
+        echo "-[hg $(hg branch)]"
+    else 
+        echo "${reset}\$"
+    fi
 }
 
 _vcs_prompt_git() {
-	#  TODO: put the picto instead of "$"
+    # Branch name
+    local STATUS="$(git status 2>&1)"
 
-	# Branch name
-	local GIT_PROMPT=$(__git_ps1 '%s')
-	# Defaut color
-	local STATE_COLOR="${blue}"
-	# Defaut picto
-	local PICTO="✔"
+    if [[ "$STATUS" != *'Not a git repository'* ]]; then
 
-	local STATUS="$(git status 2>&1)"
+        local GIT_PROMPT=$(__git_ps1 '%s')
+        # Defaut color
+        local STATE_COLOR="$blue"
+        # Defaut picto
+        local PICTO="✔"
 
-	if [[ "$STATUS" == *'Not a git repository'* ]]; then
-		echo ''
-    else
         if [[ "$STATUS" != *'working directory clean'* ]]; then
             # red if need to commit
-            STATE_COLOR="${red}"
+            STATE_COLOR="$red"
             PICTO="⚡"
+        elif [[ "$STATUS" == *'Your branch is ahead'* ]]; then
+            # cyan if need to push
+            STATE_COLOR="$cyan"
+            PICTO="↑"
+        fi
+
+        if [[ 120 -ge "$terminal_width" ]]; then
+            # Short version - without branch 
+            echo "${STATE_COLOR}${PICTO}${reset}"
         else
-            if [[ "$STATUS" == *'Your branch is ahead'* ]]; then
-                # cyan if need to push
-                STATE_COLOR="${cyan}"
-            	PICTO="↑"
-            fi
+            echo "${black}-[${STATE_COLOR}${GIT_PROMPT}${black}]${STATE_COLOR}${PICTO}${reset}"
         fi
     fi
-
-	echo "${bold}${black}-[${STATE_COLOR}${GIT_PROMPT}${black}]${black}-[${STATE_COLOR}${PICTO}${black}]${reset}"
 }
 
 
 # Prompt
 # --------------------------------------------------------------------------------------
-# 120 >= terminal_width
+# 120 >= terminal_width
 if [[ 120 -ge "$terminal_width" ]]; then
-    # Short prompt
-    export PS1='${bold}${black}[${green}\u${yellow}@${green}\h${black}][${pink}\W${black}]${reset}\$ '
+    # Short prompt
+    PS1='${bold}${black}[${green}\u${yellow}@${green}\h${black}][${pink}\W${black}]$(_end_prompt) '
 else
-    export PS1='${bold}${black}[${blue}\D{%T}${black}]-[${green}\u${yellow}@${green}\h${black}]-[${pink}\w${black}]$(_vcs_name)${reset}\$ '
+    PS1='${bold}${black}[${blue}\D{%T}${black}]-[${green}\u${yellow}@${green}\h${black}]-[${pink}\w${black}]$(_end_prompt) '
 fi
+
+export PS1
 
