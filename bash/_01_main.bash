219e864a34ec8d7404e8655b176288c9ba957adb
silvus <silvus@silvus.fr>
2014-10-13 21:52:20 +0200

    Add colors to less

--- bash/_01_main.bash
+++ bash/_01_main.bash
@@ -31,6 +31,22 @@ export EDITOR=vim
 alias vi='vim'
 alias vi_sec="vim -u $HOME/.vim/.vimrc_secure"
 
+# Less
+# -----------------------------------------------------------------------------
+export LESS_TERMCAP_mb=$(tput bold; tput setaf 4)
+export LESS_TERMCAP_md=$(tput setaf 2)
+export LESS_TERMCAP_me=$(tput sgr0)
+export LESS_TERMCAP_so=$(tput setaf 0; tput setab 2)
+export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7)
+export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+export LESS_TERMCAP_mr=$(tput rev)
+export LESS_TERMCAP_mh=$(tput dim)
+export LESS_TERMCAP_ZN=$(tput ssubm)
+export LESS_TERMCAP_ZV=$(tput rsubm)
+export LESS_TERMCAP_ZO=$(tput ssupm)
+export LESS_TERMCAP_ZW=$(tput rsupm)
+
 # Sshrc
 # -----------------------------------------------------------------------------
 if [[ -e "$HOME/.ssh/config" ]]; then
