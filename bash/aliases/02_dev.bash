9844c481827c2b9844de57d21c57a855dc235f2f
silvus <silvus@silvus.fr>
2014-12-03 13:15:57 +0100

    Add GOPATH

--- bash/aliases/02_dev.bash
+++ bash/aliases/02_dev.bash
@@ -37,3 +37,13 @@ if [[ -f "$VIRTUALENVWRAPPER_PATH" ]]; then
 	# Load virtualenvwrapper
 	source "$VIRTUALENVWRAPPER_PATH"
 fi
+
+# Golang
+# ------------------------------------------------------
+if [[ -d "/usr/local/go/bin" ]]; then
+    export PATH="$PATH:/usr/local/go/bin"
+    if [[ -d "$PROJECT_HOME/go" ]]; then
+	    export GOPATH="$PROJECT_HOME/go"
+	    export PATH="$PATH:$GOPATH/bin"
+    fi
+fi
