3a5234f06e1b0d8e26e5731ec4e8b1a102822a59
silvus <silvus@silvus.fr>
2015-01-12 22:11:53 +0100

    Play with whiptail

--- install/utilities.sh
+++ install/utilities.sh
@@ -1,29 +1,60 @@
 #!/usr/bin/env bash
 
-set -e
-
 sudo apt-get update
 
-# Installation
-# ----------------------------------------------------------------
-sudo apt-get install \
-	curl `# Play with HTTP` \
-	wget `# Downloader` \
-	rsync `# Synchronisation` \
-	htop `# Better top command` \
-	dfc `# Disk space` \
-	iftop `# Traffic htop-like` \
-	build-essential `# Compiling things` \
-	ack-grep `# Search` \
-	vim `# Text editor` \
-	xclip `# Add to clipboard` \
-	scrot `# Make screenshot` \
-	pass `# Passwords manager` \
-	feh `# Image viewer` \
-	tmux `# Split terminal` \
-	lynx `# Web browser` \
-	newsbeuter `# Rss reader`\
-	moc `# Music player` \
-	i3 `# WM` \
-	ranger highlight caca-utils `# File manager and dependencies`
+_install_server() {
+	sudo apt-get install -s \
+		curl `# Play with HTTP` \
+		wget `# Downloader` \
+		rsync `# Synchronisation` \
+		htop `# Better top command` \
+		dfc `# Disk space` \
+		iftop `# Traffic htop-like` \
+		build-essential `# Compiling things` \
+		ack-grep `# Search` \
+		vim `# Text editor` \
+		tmux `# Split terminal` \
+		lynx `# Web browser`
+}
 
+_install_desktop() {
+	sudo apt-get install -s \
+		xclip `# Add to clipboard` \
+		scrot `# Make screenshot` \
+		pass `# Passwords manager` \
+		feh `# Image viewer` \
+		newsbeuter `# Rss reader`\
+		moc `# Music player` \
+		i3 `# WM` \
+		ranger highlight caca-utils `# File manager and dependencies`
+}
+
+_install_all() {
+	_install_server
+	_install_desktop
+}
+
+OPTION=$(whiptail --title "Dotfiles" --menu "Choose your configuration" 10 50 3 \
+"1" "Server" \
+"2" "Desktop" \
+"3" "Server & Desktop" 3>&1 1>&2 2>&3)
+
+exitstatus="$?"
+if [[ "$exitstatus" == 0 ]]; then
+    case "$OPTION" in
+        1)
+            _install_server
+            ;;
+        2)
+            _install_desktop
+            ;;
+        3)
+            _install_all
+            ;;
+    esac
+else
+    echo "Abort"
+    exit 1
+fi
+
+exit 0
