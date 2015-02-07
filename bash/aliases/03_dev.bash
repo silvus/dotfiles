c229c415846d6df0cb01f91a0e82f2ee45b5f059
silvus <silvus@silvus.fr>
2015-02-07 21:20:31 +0100

    Update "dev" to search in GOPATH

--- bash/aliases/03_dev.bash
+++ bash/aliases/03_dev.bash
@@ -12,12 +12,28 @@ export DOCUMENTATION_HOME="/data/doc"
 alias server_php="php -sS localhost:8000"
 alias server_python="python3 -m http.server 8000"
 
+# Go to projects folder and git pull
+# Must be here due to "cd" subshell usage
+# ------------------------------------------------------------
 dev() {
-	if [[ -d "$PROJECT_HOME/$1" ]]; then
-		cd "$PROJECT_HOME/$1";
+	local projectname="$1"
+
+	# Check in PROJECT_HOME
+	if [[ -d "$PROJECT_HOME/${projectname}" ]]; then
+		cd "$PROJECT_HOME/${projectname}"
+		# Update project
 		if [[ -d ".git" ]]; then
 			git pull origin master
 		fi
+	# Check in GOPATH
+	elif [[ -d "$GOPATH/src/${projectname}" ]]; then
+		cd "$GOPATH/src/${projectname}"
+		# Update project
+		if [[ -d ".git" ]]; then
+			git pull origin master
+		fi
+		vim
+	# go to PROJECT_HOME
 	else
 		cd "$PROJECT_HOME"
 	fi
