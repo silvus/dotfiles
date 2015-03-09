bff191c99b4eaa9d0865da97deb71f329c9c6dde
silvus <silvus@silvus.fr>
2015-03-09 20:46:02 +0100

    Fix dev function

--- bash/aliases/03_dev.bash
+++ bash/aliases/03_dev.bash
@@ -30,15 +30,15 @@ dev() {
 	local projectname="$1"
 
 	# Check in GOPATH
-	if [[ -d "${GOPATH}/src/${projectname}" ]]; then
+	if [[ ! -z "$projectname" ]] && [[ -d "${GOPATH}/src/${projectname}" ]]; then
 		cd "${GOPATH}/src/${projectname}"
 		# Update project
 		if [[ -d ".git" ]]; then
 			git pull origin master
 		fi
+		vim -u "${HOME}/.vim/.vimrc_ide"
 	# go to GOPATH
 	else
 		cd "${GOPATH}/src"
 	fi
-	vim -u "${HOME}/.vim/.vimrc_ide"
 }
