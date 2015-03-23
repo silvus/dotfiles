07e31985d422336ce3d65880a02f21c9ddd3aabb
silvus <silvus@silvus.fr>
2015-03-23 11:55:14 +0100

    Reorder

--- git/git_configure.bash
+++ git/git_configure.bash
@@ -2,7 +2,7 @@
 
 # Configure git
 # -----------------------------------------------------------------------------
-git config --global user.name "silvus"
+# git config --global user.name "silvus"
 # git config --global user.email "silvus@MAIL.com"
 
 git config --global color.ui "auto"
@@ -11,14 +11,13 @@ git config --global core.editor "vim"
 git config --global core.autocrlf "input"
 
 git config --global alias.st "status"
-git config --global alias.co "commit -a"
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
-git config --global alias.gr "grep -p"
 git config --global alias.aa "add --all"
-git config --global alias.co "commit -a"
+git config --global alias.co "commit -av"
+git config --global alias.gr "grep -p"
 git config --global alias.pusom "push origin master"
 git config --global alias.pulom "pull origin master"
 git config --global alias.dif "diff --stat=160,120"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 
 git config --global web.browser "lynx"
 
