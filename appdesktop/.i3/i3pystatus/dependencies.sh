4fc3411b8d204cd5ab3ea578d1b3bbda0836d2fc
silvus <silvus@silvus.fr>
2015-10-26 23:46:48 +0100

    Back to i3status. Mouve i3 to ~/.config

--- appdesktop/.i3/i3pystatus/dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~/.i3/i3pystatus
-
-pip3 install i3pystatus netifaces colour psutil pyalsaaudio --upgrade -t .pip/
--- appdesktop/.i3/i3pystatus/launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~/.i3
-export PYTHONPATH="./.pip:$PYTHONPATH"
-
-python3 ~/.i3/i3pystatus/bar.py
