1cc06cf1ac62642fc6a28fa5b526b00d39919a92
silvus <silvus@silvus.fr>
2015-11-02 11:57:03 +0100

    Add xsession to set locales

--- bash/aliases/01_main.bash
+++ bash/aliases/01_main.bash
@@ -12,7 +12,7 @@ mkcd() { mkdir -p "$@" && cd "$_"; }
 
 # Misc
 # -----------------------------------------------------------------------------
-alias l='LC_ALL=C ls -lhaF --color=auto --group-directories-first'
+alias l='ls -lhaF --color=auto --group-directories-first'
 alias rm='rm -I' # prompt if deleting more than 3 files at a time
 alias o='xdg-open'
 alias diskfree='df -H'
@@ -20,14 +20,13 @@ alias diskusage='ncdu'
 alias resolution='xrandr -q | grep "*" | cut -d " " -f 4'
 alias distro='cat /etc/issue'
 alias shortcuts='bind -P'
-alias git='LC_ALL=en_US git'
 alias sudosu="sudo -Es"
 # Searches for duplicates files (size and md5)
 alias duplicate='find -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate'
 alias process='ps aux | grep'
 alias ports='netstat -tulanp'
 alias tcpdump='tcpdump -i eth0'
-alias internetusage='lsof -P -i -n'
+alias netusage='lsof -P -i -n'
 alias firewalllist='sudo iptables -L -n -v --line-numbers'
 alias mountshow='mount |column -t'
 alias calculator='bc -l'
