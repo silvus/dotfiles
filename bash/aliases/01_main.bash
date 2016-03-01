2c1c0a14f6b23494d8acd4ad165c8708d21efb48
silvus <silvus@silvus.fr>
2016-03-01 13:07:45 +0100

    Data storage consolidation

--- bash/aliases/01_main.bash
+++ bash/aliases/01_main.bash
@@ -29,7 +29,6 @@ alias netusage='lsof -P -i -n'
 alias firewalllist='sudo iptables -L -n -v --line-numbers'
 alias mountshow='mount |column -t'
 alias calculator='bc -l'
-alias calendar='calcurse --calendar /data/doc/.calendar/apts'
 alias lynx="lynx -cfg=$HOME/.lynx/lynxrc -lss=$HOME/.lynx/lynx.lss"
 alias tmux="tmux -2" # Getting 256 colors to work in tmux
 
