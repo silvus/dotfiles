ce64aee8a0453791c263c0e14afdc0aeb3d6ca42
silvus <silvus@silvus.fr>
2017-08-07 00:47:12 +0200

    Awesome wm: mpv without titlebar

--- appdesktop/.config/awesome/rc.lua
+++ appdesktop/.config/awesome/rc.lua
@@ -739,15 +739,15 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
+		properties = { border_width = beautiful.border_width,
+		             border_color = beautiful.border_normal,
+		             focus = awful.client.focus.filter,
+		             raise = true,
+		             keys = clientkeys,
+		             buttons = clientbuttons,
+		             screen = awful.screen.preferred,
+		             placement = awful.placement.no_overlap+awful.placement.no_offscreen
+		}
     },
 
     -- Floating clients.
@@ -774,16 +774,26 @@ awful.rules.rules = {
           "AlarmWindow",  -- Thunderbird's calendar.
           -- "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
         }
-      }, properties = { floating = true }},
+      }, properties = { floating = true }
+    },
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
+		}, properties = { titlebars_enabled = true }
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+
+    { rule = { class = "mpv" },
+		properties = {
+			floating = false,
+			titlebars_enabled = false,
+			sticky = true,
+			ontop = true,
+		}
+    },
 }
 -- }}}
 
