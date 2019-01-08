90e02b063ac654aca74c970b405846587b6353d4
silvus <silvus@silvus.fr>
2019-01-08 16:29:00 +0100

    Color man page: use most instead of less env variables

--- shell/aliases/01_main.sh
+++ shell/aliases/01_main.sh
@@ -48,26 +48,13 @@ alias keymapqwerty="setxkbmap -model pc105 -layout us,fr -variant oss"
 
 # Man
 # -----------------------------------------------------------------------------
-# Don’t clear the screen after quitting a manual page
-export MANPAGER="less -X";
-# Get colors in manual pages
-man() {
-	env \
-		LESS_TERMCAP_mb=$(tput bold; tput setaf 4) \
-		LESS_TERMCAP_md=$(tput setaf 2) \
-		LESS_TERMCAP_me=$(tput sgr0) \
-		LESS_TERMCAP_so=$(tput setaf 0; tput setab 2) \
-		LESS_TERMCAP_se=$(tput rmso; tput sgr0) \
-		LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7) \
-		LESS_TERMCAP_ue=$(tput rmul; tput sgr0) \
-		LESS_TERMCAP_mr=$(tput rev) \
-		LESS_TERMCAP_mh=$(tput dim) \
-		LESS_TERMCAP_ZN=$(tput ssubm) \
-		LESS_TERMCAP_ZV=$(tput rsubm) \
-		LESS_TERMCAP_ZO=$(tput ssupm) \
-		LESS_TERMCAP_ZW=$(tput rsupm) \
-		man "$@"
-}
+if [[ -x "$(which most)" ]]; then
+	# Color pager
+	export MANPAGER="most";
+else
+	# Don’t clear the screen after quitting a manual page
+	export MANPAGER="less -X";
+fi
 
 # Vim
 # -----------------------------------------------------------------------------
