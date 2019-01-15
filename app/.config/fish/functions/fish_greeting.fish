48d64833c84ea79da3b02c0e9199c05e418ee8df
silvus <silvus@silvus.fr>
2019-01-15 16:08:38 +0100

    Improve fish greeting

--- app/.config/fish/functions/fish_greeting.fish
+++ app/.config/fish/functions/fish_greeting.fish
@@ -16,12 +16,12 @@ function fish_logo \
     set i (set_color $inner_color)
 
     echo '                 '$o'___
-  ___======____='$m'-'$i'-'$m'-='$o')
-/T            \_'$i'--='$m'=='$o')
-'$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')
- \      / )J'$m'~~    '$o'\\'$i'-='$o')
-  \\\\___/  )JJ'$m'~'$i'~~   '$o'\)
-   \_____/JJJ'$m'~~'$i'~~    '$o'\\
+  ___======____='$m'-'$i'-'$m'-='$o')                     '$o$USER'@'(hostname -s)$o'
+/T            \_'$i'--='$m'=='$o')            '$m'IP      '$i(hostname  -I | cut -f1 -d' ')$o'
+'$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')            '$m'Uptime  '$i(uptime -p | sed 's/[^ _-]*/\u&/g')$o'
+ \      / )J'$m'~~    '$o'\\'$i'-='$o')            '$m'Kernel  '$i(uname -s) (uname -m) (uname -r)$o'
+  \\\\___/  )JJ'$m'~'$i'~~   '$o'\)             '$m'Disk /  '$i(df --output=pcent  / | sed -nr '/[[:digit:]]/{s/[[:space:]]+([[:digit:]]+)%/\1/;p}')'%'$o'
+   \_____/JJJ'$m'~~'$i'~~    '$o'\\            '$m'Date    '$i(date | sed 's/[^ _-]*/\u&/g')$o'
    '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\
   (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_
   '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__
@@ -36,3 +36,4 @@ end
 function fish_greeting
 	fish_logo
 end
+
