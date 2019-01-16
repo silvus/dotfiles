144ecb548619d490d79026a65c1c5711a040e638
silvus <silvus@silvus.fr>
2019-01-16 11:56:52 +0100

    Add textadept ressources

--- app/.textadept/init.lua
+++ app/.textadept/init.lua
@@ -1,3 +1,5 @@
+-- https://notabug.org/reback00/ta-config/src/master/init.lua
+
 local textredux = require('textredux')
 textredux.hijack()
 
@@ -5,6 +7,12 @@ events.connect(events.LEXER_LOADED, function(lexer)
 	-- Tabs
 	buffer.tab_width = 4
 	buffer.use_tabs = true
+	-- show trailing whitespace
+	-- buffer.view_ws = buffer.WS_VISIBLEAFTERINDENT
+	-- wrap long lines, always
+	buffer.wrap_mode = buffer.WRAP_WHITESPACE
+	-- paste on multiple cursor places
+	buffer.multi_paste = buffer.MULTIPASTE_EACH
 
 	if lexer == 'markdown' then
 		-- No auto pairing in markdown
@@ -31,10 +39,17 @@ textadept.run.run_commands.python = 'python3 "%f"'
 keys['cP'] = textadept.menu.select_command
 keys['ct'] = buffer.new
 keys['cd'] = buffer.line_delete
+keys['cg'] = textadept.editing.goto_line -- ctrl + g
 
 local m_buffer = textadept.menu.menubar[_L['_Buffer']]
 keys['cpgdn'] = m_buffer[_L['_Next Buffer']][2]
 keys['cpgup'] = m_buffer[_L['_Previous Buffer']][2]
+
+local m_tools = textadept.menu.menubar[_L['_Tools']]
+--keys[not OSX and (GUI and 'ce' or 'mc') or 'me'] = m_tools[_L['Command _Entry']][2]
+--keys[not OSX and (GUI and 'cE' or 'mC') or 'mE'] = m_tools[_L['Select Co_mmand']][2]
+keys['cX'] = m_tools[_L['Command _Entry']][2]
+
 
 -- Already on default
 -- keys['cE'] = Command Selection
@@ -42,4 +57,25 @@ keys['cp'] = io.open_recent_file
 -- shows buffers by their z-order (most recently viewed to least recently viewed
 -- keys['ce'] = function() ui.switch_buffer(true) end
 keys['ce'] = textredux.buffer_list.show
+
+
+-- highlight trailing whitespace
+-- from: https://foicica.com/wiki/highlight-trailing-whitespace
+local tw_indicator = _SCINTILLA.next_indic_number()
+buffer.indic_style[tw_indicator] = buffer.INDIC_ROUNDBOX
+buffer.indic_fore[tw_indicator] = 0x0000FF
+events.connect(events.UPDATE_UI, function(updated)
+	if updated ~= buffer.UPDATE_CONTENT then return end
+	buffer.target_start = 0
+	buffer.search_flags = buffer.FIND_REGEXP
+	buffer.indicator_current = tw_indicator
+	buffer:indicator_clear_range(0, buffer.length)
+	while true do
+	buffer.target_end = buffer.length
+	if buffer:search_in_target('[ \t]+$') == -1 then break end
+		buffer:indicator_fill_range(
+		buffer.target_start, buffer.target_end - buffer.target_start)
+		buffer.target_start = buffer.target_end
+	end
+end)
 
