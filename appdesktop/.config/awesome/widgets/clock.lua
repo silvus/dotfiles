2c0bc1ee21287f6e384fe702127dd28da1aacaff
silvus <silvus@silvus.fr>
2019-10-18 09:36:42 +0200

    AwesomeWm: add tooltip to clock widget

--- appdesktop/.config/awesome/widgets/clock.lua
+++ appdesktop/.config/awesome/widgets/clock.lua
@@ -1,6 +1,8 @@
 local beautiful = require("beautiful")
 local wibox = require("wibox")
+local awful = require("awful")
 local calendar = require("utils.calendar")
+local globalos = require("os")
 
 local customwidget = {}
 
@@ -8,16 +10,19 @@ customwidget.icon = wibox.widget.imagebox(beautiful.clock)
 
 -- Textclock widget with calendar
 
-customwidget.widget = wibox.widget.textclock("%a %d %b  <span color='#ffffff'>%H:%M:%S</span>", 1)
+-- customwidget.widget = wibox.widget.textclock("%a %d %b  <span color='#ffffff'>%H:%M:%S</span>", 1)
+customwidget.widget = wibox.widget.textclock("%a %d %b  <span color='#ffffff'>%T</span>", 1)
+
+-- Tooltip
+local widget_tooltip = awful.tooltip {
+	objects        = { customwidget.widget, customwidget.icon },
+	timer_function = function()
+		return globalos.date('%A %d %B %Y\n%T')
+	end,
+}
 
 -- attach it as popup to your text clock widget:
 calendar({}):attach(customwidget.widget)
 calendar({}):attach(customwidget.icon)
 
 return customwidget
-
-
-
-
-
-
