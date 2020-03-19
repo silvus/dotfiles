3033cdc8b2ffdc633183fd25abaa4f52df3f0a51
silvus <silvus@silvus.fr>
2020-03-19 14:46:24 +0100

    AwesomeWm: fix VPN widget

--- appdesktop/.config/awesome/widgets/vpn.lua
+++ appdesktop/.config/awesome/widgets/vpn.lua
@@ -4,24 +4,29 @@ local wibox = require("wibox")
 
 local customwidget = {}
 
+customwidget.vpn_text = wibox.widget.textbox()
+
 customwidget.icon = wibox.widget.imagebox(beautiful.net_wired)
 customwidget.icon.visible = false
 
 -- VPN
 
-local vpn = awful.widget.watch(
+awful.widget.watch(
 	"ip addr show tun0",
 	10,
 	function(customwidget, stdout, stderr, exitreason, exitcode)
 		if exitcode == 0 then
-			customwidget:set_markup("<span color='" .. beautiful.success .. "'>VPN: ON</span>")
+			customwidget.vpn_text:set_markup("<span font='DejaVu Sans 8' color='" .. beautiful.success .. "'>VPN</span>")
 			customwidget.icon.visible = true
 		else
-			customwidget:set_markup("")
+			customwidget.vpn_text:set_markup("")
 			customwidget.icon.visible = false
 		end
-	end
+		return
+	end,
+	customwidget
 )
-customwidget.widget = wibox.container.margin(vpn, 2, 7, 4, 4)
+
+customwidget.widget = wibox.container.margin(customwidget.vpn_text, 1, 1, 1, 1)
 
 return customwidget
