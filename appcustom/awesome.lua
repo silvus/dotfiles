c6759faaca8656c2c625d65d77933bfe0f6cba48
silvus <silvus@silvus.fr>
2021-10-01 17:12:22 +0200

    Generate ~/.dotfiles_custom files

---
+++ appcustom/awesome.lua
@@ -0,0 +1,12 @@
+local config = {}
+
+-- config.theme = "lagoon"
+-- config.theme = "matrix"
+
+-- Videos windows placement (auto, float, fullscreen)
+-- config.rules_videos = "auto"
+
+-- Set a different wallpaper for each tag (and each screen), else take a random on init
+-- config.wallpapers_by_tag = false
+
+return config
