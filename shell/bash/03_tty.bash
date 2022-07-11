fc2b5f522fc1c116dd2c3112d44f020f7df87d2c
silvus <silvus@silvus.fr>
2022-07-11 19:42:36 +0200

    Fix TTY blue

--- shell/bash/03_tty.bash
+++ shell/bash/03_tty.bash
@@ -26,7 +26,7 @@ if [ "$TERM" = "linux" ]; then
     printf '\033]P1991f1f'; # red
     printf '\033]P25c991f'; # green
     printf '\033]P3997b1f'; # yellow
-    printf '\033]P41f3e99'; # blue
+    printf '\033]P41f5c99'; # blue
     printf '\033]P5991f70'; # magenta
     printf '\033]P61f9999'; # cyan
     printf '\033]P7ccbc95'; # white
@@ -34,7 +34,7 @@ if [ "$TERM" = "linux" ]; then
     printf '\033]P9E62E2E'; # brighter red
     printf '\033]PA8AE62E'; # brighter green
     printf '\033]PBE6B82E'; # brighter yellow
-    printf '\033]PC2E5CE6'; # brighter blue
+    printf '\033]PC2ea3e6'; # brighter blue
     printf '\033]PDE62EA9'; # brighter magenta
     printf '\033]PE2EE6E6'; # brighter cyan
     printf '\033]PFE6D7AB'; # brighter white
