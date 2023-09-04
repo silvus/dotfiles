cd9f96a76b152dd8ce16e36bc6a2c9b4076634f9
silvus <silvus@silvus.fr>
2023-09-04 09:26:11 +0200

    Add orgmode keywords

--- app/.config/nvim/after/plugin/orgmode.lua
+++ app/.config/nvim/after/plugin/orgmode.lua
@@ -1,9 +1,15 @@
 require('orgmode').setup({
 	org_agenda_files = { '/data/doc/orgmode/**/*' },
 	org_default_notes_file = '/data/doc/orgmode/todo.org',
+	org_indent_mode = 'noindent',
+	org_todo_keywords = {'TODO(t)', 'NEXT(n)', 'WAIT(w@/!)', '|', 'DONE(d!)', 'CANCELED(c@)', 'DELEGATED(g@)', 'INACTIVE(i@)'},
+	win_split_mode = 'auto',
+	org_log_into_drawer = 'LOGBOOK',
+	org_log_done = 'time',
 })
 
 
 
 -- Load custom treesitter grammar for org filetype
 require('orgmode').setup_ts_grammar()
+
