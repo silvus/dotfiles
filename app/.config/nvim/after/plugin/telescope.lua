b2f3b294adfb4324d32d8a4e91cbb6262b1502a7
silvus <silvus@silvus.fr>
2023-12-04 22:54:21 +0100

    Nvim: Add bindings

--- app/.config/nvim/after/plugin/telescope.lua
+++ app/.config/nvim/after/plugin/telescope.lua
@@ -18,8 +18,8 @@ require("telescope").setup {
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<C-e>', builtin.oldfiles, {})
+vim.keymap.set({'n', 'i', 'v'}, '<C-p>', builtin.git_files, {})
+vim.keymap.set({'n', 'i', 'v'}, '<C-e>', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fsp', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
--- app/.config/nvim/lua/keymap.lua
+++ app/.config/nvim/lua/keymap.lua
@@ -4,3 +4,17 @@ vim.g.maplocalleader = ' '
 
 -- Netw
 vim.keymap.set('n', "<leader>pv", vim.cmd.Ex)
+
+-- Delete line
+vim.keymap.set('n', '<C-d>', 'dd', { silent = true, desc = 'Delete line' })
+vim.keymap.set({'i', 'v'}, '<C-d>', '<C-o>dd', { silent = true, desc = 'Delete line' })
+
+-- Save
+vim.keymap.set('n', '<C-s>', ':update<cr>', { silent = true, desc = 'Save' })
+vim.keymap.set({'i', 'v'}, '<C-s>', '<C-o>:update<cr>', { silent = true, desc = 'Save' })
+
+-- Close
+-- vim.keymap.set('n', '<C-q>', ':q<cr>', { silent = true, desc = 'Close' })
+
+-- Indent
+vim.keymap.set({'i', 'v'}, '<S-Tab>', '<C-o><<', { silent = true, desc = 'Unindent' })
