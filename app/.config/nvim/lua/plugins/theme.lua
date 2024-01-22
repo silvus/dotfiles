70bb750c42c0f646bd6971b79b564682c55aeab1
silvus <silvus@silvus.fr>
2024-01-22 01:22:48 +0100

    Nvim: show relative filename

--- app/.config/nvim/lua/plugins/theme.lua
+++ app/.config/nvim/lua/plugins/theme.lua
@@ -42,6 +42,19 @@ return {
 					theme = 'codedark',
 					-- icons_enabled = false,
 				},
+				sections = {
+					lualine_c = {
+						{
+							'filename',
+							newfile_status = true,  -- Display new file status (new file means no write after created)
+							path = 1,               -- 0: Just the filename
+													-- 1: Relative path
+													-- 2: Absolute path
+													-- 3: Absolute path, with tilde as the home directory
+													-- 4: Filename and parent dir, with tilde as the home directory
+						}
+					}
+				},
 			})
 		end
 	},
