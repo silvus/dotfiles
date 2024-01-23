6db5f9a38574481bd29e1e3b6720bbff5fd8838a
silvus <silvus@silvus.fr>
2024-01-23 16:14:17 +0100

    Nvim: Fix note custom command opening on netrw

--- app/.config/nvim/lua/notes.lua
+++ app/.config/nvim/lua/notes.lua
@@ -1,22 +1,46 @@
 -- Create a command and a binding to open today's note.
 function NoteOpenToday()
-	-- TODO: get note dir path from en variable
-	local today_note_path = '/tmp/test/' .. os.date("%Y-%m-%d") .. '.md'
+	local note_dir = (os.getenv('NOTE_PATH') or os.getenv('SILVUSDOC'))
+	local today_note_path = note_dir .. '/journalogs/' .. os.date('%Y') .. '/' .. os.date('%m') .. '/' .. os.date('%Y-%m-%d') .. '.md'
 
 	-- Create parents folders if needed
 	if not vim.loop.fs_stat(today_note_path) then
-		vim.fn.mkdir(vim.fn.fnamemodify(today_note_path, ":p:h"), "p")
+		vim.fn.mkdir(vim.fn.fnamemodify(today_note_path, ':p:h'), 'p')
 	end
 
+	-- cd to note folder
+	vim.api.nvim_set_current_dir(note_dir)
+
+	-- Open note
 	vim.cmd.edit(today_note_path)
-
-	-- TODO: cd to doc folder
-	-- vim.api.nvim_set_current_dir(client.config.root_dir)
-
 end
 
-vim.keymap.set('n', '<C-l>', NoteOpenToday , { silent = true, desc = 'Open today\'s note' })
+-- Command to open Today's Note
 vim.api.nvim_create_user_command('NoteToday', NoteOpenToday, {})
 
+-- Global binding to open Today's Note
+vim.keymap.set('n', '<C-l>', NoteOpenToday , { silent = false, desc = 'Open today\'s note' })
+vim.api.nvim_create_autocmd('filetype', {
+	pattern = 'netrw',
+	desc = 'Bind NoteOpenToday on netrw',
+	callback = function()
+		local bind = function(lhs, rhs)
+			vim.keymap.set('n', lhs, rhs, {remap = true, buffer = true})
+		end
+		-- Ctrl-l is "refresh" on netrw
+		bind( '<C-l>', NoteOpenToday)
+	
+		-- edit new file
+		-- bind('n', '%')
+	
+		-- rename file
+		-- bind('r', 'R')
+	end
+})
+
 -- Ensure that changes to buffers are saved when you navigate away from that buffer
-vim.api.nvim_create_autocmd("FileType", {pattern = "markdown", command = "set awa"})
+vim.api.nvim_create_autocmd('filetype', {
+	pattern = 'markdown',
+	desc = 'Ensure that changes to buffers are saved when you navigate away from that buffer',
+	command = 'set awa',
+})
--- app/.config/nvim/lua/plugins/telescope.lua
+++ app/.config/nvim/lua/plugins/telescope.lua
@@ -27,7 +27,7 @@ return {
 
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fi', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
 			vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 			vim.keymap.set({'n', 'i', 'v'}, '<C-p>', builtin.find_files, {})
 			vim.keymap.set({'n', 'i', 'v'}, '<C-e>', builtin.oldfiles, {})
