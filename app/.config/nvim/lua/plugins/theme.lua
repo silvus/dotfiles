0f372181204edc2bb578ccbe7aa426d999bf0baf
silvus <silvus@silvus.fr>
2024-01-24 02:26:13 +0100

    Nvim: Add Notify

--- app/.config/nvim/lua/plugins/theme.lua
+++ app/.config/nvim/lua/plugins/theme.lua
@@ -29,25 +29,21 @@ return {
 	{
 		'nvim-lualine/lualine.nvim',
 		-- and https://github.com/ryanoasis/nerd-fonts
-		dependencies = 'nvim-tree/nvim-web-devicons',
+		dependencies = {
+			'nvim-tree/nvim-web-devicons',
+		},
 		event = "VeryLazy",
 		config = function()
-			require('nvim-web-devicons').setup()
-
-			-- table.insert(opts.icons_enabled, false)
-			-- table.insert(opts.theme, 'codedark')
-
 			require('lualine').setup({
 				options = {
 					theme = 'codedark',
-					-- icons_enabled = false,
 				},
 				sections = {
 					lualine_c = {
 						{
 							'filename',
-							newfile_status = true,  -- Display new file status (new file means no write after created)
-							path = 1,               -- 0: Just the filename
+							newfile_status = true,	-- Display new file status (new file means no write after created)
+							path = 1,				-- 0: Just the filename
 													-- 1: Relative path
 													-- 2: Absolute path
 													-- 3: Absolute path, with tilde as the home directory
@@ -57,6 +53,15 @@ return {
 				},
 			})
 		end
+	},
+	{
+		'rcarriga/nvim-notify',
+		config = function()
+			require("notify").setup({
+				background_colour = "#000000",
+			})
+			vim.notify = require("notify")
+		end,
 	},
 	{
 		"lukas-reineke/headlines.nvim",
