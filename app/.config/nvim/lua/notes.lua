9974aae21478c37f9ce8e0a7cb8440218f15d948
silvus <silvus@silvus.fr>
2024-01-24 02:27:46 +0100

    Nvim: Add NoteExportToPDF custom command

--- app/.config/nvim/lua/notes.lua
+++ app/.config/nvim/lua/notes.lua
@@ -15,8 +15,41 @@ function NoteOpenToday()
 	vim.cmd.edit(today_note_path)
 end
 
+function NoteExportToPDF()
+	-- Require: pandoc texlive-latex-base texlive-latex-recommended wkhtmltopdf
+	local Job = require('plenary.job')
+
+	local current_file_path = vim.fn.expand('%:p')
+	local target_file_path = vim.fn.expand('%:r') .. '.pdf'
+
+	Job:new({
+		command = '/usr/bin/pandoc',
+		args = {
+			'--standalone',
+			'--from=gfm+hard_line_breaks', -- GitHub-Flavored Markdown + Keep line break
+			'--pdf-engine=wkhtmltopdf',
+			'--template','eisvogel',
+			current_file_path,
+			'-o',
+			target_file_path,
+			'--metadata', 'title=' .. vim.fn.expand('%:t'),
+			'--metadata', 'mainfont="DejaVu Serif"',
+			'--metadata', 'monofont="DejaVu Sans Mono"',
+		},
+		cwd = '/tmp',
+		on_exit = function(j, return_val)
+			if return_val == 0 then
+				vim.notify('Pandoc conversion complete to ' .. target_file_path, 'succes', {title = 'Pandoc'})
+			else
+				vim.notify('Pandoc conversion error: ' .. table.concat(j:stderr_result(), '\n'), 'error', {title = 'Pandoc'})
+			end
+		end,
+	}):sync() -- or start()
+end
+
 -- Command to open Today's Note
 vim.api.nvim_create_user_command('NoteToday', NoteOpenToday, {})
+vim.api.nvim_create_user_command('NoteExportToPDF', NoteExportToPDF, {})
 
 -- Global binding to open Today's Note
 vim.keymap.set('n', '<C-l>', NoteOpenToday , { silent = false, desc = 'Open today\'s note' })
