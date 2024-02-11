89adf523f17a3423c95b1e8f99d1c5bab1bc947a
silvus <silvus@silvus.fr>
2024-02-11 23:21:56 +0100

    Nvim: Fix markdown list items creation

--- app/.config/nvim/lua/plugins/notes.lua
+++ app/.config/nvim/lua/plugins/notes.lua
@@ -23,6 +23,13 @@ return {
 				to_do = {
 					symbols = {' ', 'X', '-'},
 				},
+				mappings = {
+					MkdnEnter = {{'i', 'n', 'v'}, '<CR>'} -- This monolithic command has the aforementioned
+						-- insert-mode-specific behavior and also will trigger row jumping in tables. Outside
+						-- of lists and tables, it behaves as <CR> normally does.
+					-- MkdnNewListItem = {'i', '<CR>'} -- Use this command instead if you only want <CR> in
+						-- insert mode to add a new list item (and behave as usual outside of lists).
+				}
 			})
 		end
 	},
