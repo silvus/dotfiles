6702131f712850f794633d06a5fffadb413d56ca
silvus <silvus@silvus.fr>
2024-11-06 01:39:56 +0100

    Remove specific layout

--- appdesktop/.config/awesome/desktops.lua
+++ appdesktop/.config/awesome/desktops.lua
@@ -24,7 +24,7 @@ local function init(s)
 			{
 				name   = tags_names[3],
 				icon   = beautiful.code,
-				layout = awful.layout.suit.fair,
+				-- layout = awful.layout.suit.fair,
 			},
 			{
 				name   = tags_names[4],
@@ -45,8 +45,8 @@ local function init(s)
 			},
 			{
 				name   = tags_names[8],
-				-- layout = awful.layout.suit.max,
 				icon   = beautiful.fire,
+				-- layout = awful.layout.suit.max,
 			},
 			{
 				name   = tags_names[9],
@@ -75,43 +75,8 @@ local function init(s)
 	end
 end
 
--- Use to launch program only one time
--- local launched_list = {}
-
--- -- This function will run once every time Awesome is started, only one time per programm (https://github.com/lcpz/awesome-copycats/blob/master/rc.lua.template)
--- local function run_once(cmd_arr)
--- 	for _, cmd in ipairs(cmd_arr) do
--- 		-- if not already launched
--- 		if launched_list[cmd] == nil then
--- 			-- Add to programms launched list
--- 			launched_list[cmd] = true
-
--- 			-- Doesn't work with symlinks
--- 			-- awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
--- 			awful.spawn.with_shell(string.format("pgrep -u $USER \"$(basename %s)\" > /dev/null || (%s)", cmd, cmd))
--- 		end
--- 	end
--- end
-
--- -- Switch tag event
--- local function switch(t)
--- 	if t.selected then
--- 		-- Auto launch programms on tag access
--- 		if t.name == tags_names[1] then
--- 			run_once({"firefox"})
--- 		elseif t.name == tags_names[2] then
--- 			run_once({"/usr/share/codium/bin/codium"})
--- 		elseif t.name == tags_names[3] then
--- 			run_once({"thunderbird"})
--- 		elseif t.name == tags_names[4] then
--- 			run_once({"pcmanfm"})
--- 		end
--- 	end
--- end
-
 
 desktops.init = init
--- desktops.switch = switch
 desktops.tags_names = tags_names
 
 return desktops
