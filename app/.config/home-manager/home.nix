ee700bd7ebe024335e71b15520bd5e59154731f4
silvus <silvus@silvus.fr>
2025-03-25 00:53:53 +0100

    Try home manager again

--- app/.config/home-manager/home.nix
+++ app/.config/home-manager/home.nix
@@ -19,16 +19,24 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
+
+    # Cat alternative
     bat
+    # Find alternative
     fd
+    # Code statistics
     tokei
+    # File manager
+    yazi
+    # Git TUI
+    lazygit
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
