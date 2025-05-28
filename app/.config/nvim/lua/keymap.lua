58a399959e429092e449dfb95549a7f593d2b41e
silvus <silvus@silvus.fr>
2025-05-28 14:50:54 +0200

    Use leader prefix instead

--- app/.config/nvim/lua/keymap.lua
+++ app/.config/nvim/lua/keymap.lua
@@ -24,19 +24,19 @@ vim.keymap.set({'i', 'v'}, '<S-Tab>', '<C-o><<', { silent = true, desc = 'Uninde
 -- vim.keymap.set("n", "<leader>s", ":vsplit<cr>", { noremap = true, silent = true }) -- vertical split
 
 -- Window navigation
-vim.keymap.set("n", "<C-Right>", "<C-w>l", { noremap = true, silent = true }) -- move to right window
-vim.keymap.set("n", "<C-Left>", "<C-w>h", { noremap = true, silent = true }) -- move to left window
-vim.keymap.set("n", "<C-Down>", "<C-w>j", { noremap = true, silent = true }) -- move to lower window
-vim.keymap.set("n", "<C-Up>", "<C-w>k", { noremap = true, silent = true }) -- move to upper window
-vim.keymap.set("i", "<C-Right>", "<C-o><C-w>l", { noremap = true, silent = true }) -- move to right window and stay in insert mode
-vim.keymap.set("i", "<C-Left>", "<C-o><C-w>h", { noremap = true, silent = true }) -- move to left window and stay in insert mode
-vim.keymap.set("i", "<C-Down>", "<C-o><C-w>j", { noremap = true, silent = true }) -- move to right window and stay in insert mode
-vim.keymap.set("i", "<C-Up>", "<C-o><C-w>k", { noremap = true, silent = true }) -- move to left window and stay in insert mode
+vim.keymap.set("n", "<leader><Right>", "<C-w>l", { noremap = true, silent = true }) -- move to right window
+vim.keymap.set("n", "<leader><Left>", "<C-w>h", { noremap = true, silent = true }) -- move to left window
+vim.keymap.set("n", "<leader><Down>", "<C-w>j", { noremap = true, silent = true }) -- move to lower window
+vim.keymap.set("n", "<leader><Up>", "<C-w>k", { noremap = true, silent = true }) -- move to upper window
+-- vim.keymap.set("i", "<leader><Right>", "<C-o><C-w>l", { noremap = true, silent = true }) -- move to right window and stay in insert mode
+-- vim.keymap.set("i", "<leader><Left>", "<C-o><C-w>h", { noremap = true, silent = true }) -- move to left window and stay in insert mode
+-- vim.keymap.set("i", "<leader><Down>", "<C-o><C-w>j", { noremap = true, silent = true }) -- move to right window and stay in insert mode
+-- vim.keymap.set("i", "<leader><Up>", "<C-o><C-w>k", { noremap = true, silent = true }) -- move to left window and stay in insert mode
 
 -- Window resizing
-vim.keymap.set("n", "<C-S-Right>", "<C-w>>", { noremap = true, silent = true }) -- increase window width
-vim.keymap.set("n", "<C-S-Left>", "<C-w><", { noremap = true, silent = true }) -- decrease window width
-vim.keymap.set("n", "<C-S-Down>", "<C-w>+", { noremap = true, silent = true }) -- increase window height
-vim.keymap.set("n", "<C-S-Up>", "<C-w>-", { noremap = true, silent = true }) -- decrease window height
-vim.keymap.set("i", "<C-S-Right>", "<C-o><C-w>>", { noremap = true, silent = true }) -- increase window width
-vim.keymap.set("i", "<C-S-Left>", "<C-o><C-w><", { noremap = true, silent = true }) -- decrease window width
+vim.keymap.set("n", "<leader><S-Right>", "<C-w>>", { noremap = true, silent = true }) -- increase window width
+vim.keymap.set("n", "<leader><S-Left>", "<C-w><", { noremap = true, silent = true }) -- decrease window width
+vim.keymap.set("n", "<leader><S-Down>", "<C-w>+", { noremap = true, silent = true }) -- increase window height
+vim.keymap.set("n", "<leader><S-Up>", "<C-w>-", { noremap = true, silent = true }) -- decrease window height
+-- vim.keymap.set("i", "<leader><S-Right>", "<C-o><C-w>>", { noremap = true, silent = true }) -- increase window width
+-- vim.keymap.set("i", "<leader><S-Left>", "<C-o><C-w><", { noremap = true, silent = true }) -- decrease window width
