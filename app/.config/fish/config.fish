e0060714a3d3dc3befdae1a2ab02168a6c88e506
silvus <silvus@silvus.fr>
2025-07-18 23:26:22 +0200

    Change TTY color scheme

--- app/.config/fish/config.fish
+++ app/.config/fish/config.fish
@@ -138,46 +138,46 @@ bind \ch fzf-history-widget
 # bind \ct 'tmux_sessionizer -p "/data/doc/todo.org"'
 
 
-# TTY theme (https://www.reddit.com/r/unixporn/comments/55kjdb/tmux_tty_with_custom_theme/)
+# TTY colour scheme (Tango like)
 # -----------------------------------------------------------------------------
-# /*COLOR         DESC.         H    S   l */
-# "#1A1813",  /*  0: black     45,  25, 10 */
-# "#991f1f",  /*  1: red        0,  80, 60 */
-# "#5C991F",  /*  2: green     90,  80, 60 */
-# "#997B1F",  /*  3: yellow    45,  80, 60 */
-# "#2e53bf",  /*  4: blue     225,  80, 60 */
-# "#991F70",  /*  5: magenta  320,  80, 60 */
-# "#1F9999",  /*  6: cyan     180,  80, 60 */
-# "#e6e6e6",  /*  7: white     45,  25, 80 */
-#
-# /*Brighter colors*/
-# "#333026",  /*  0: black     45,  25,  20 */
-# "#E62E2E",  /*  1: red        0,  100, 80 */
-# "#8AE62E",  /*  2: green     90,  100, 80 */
-# "#E6B82E",  /*  3: yellow    45,  100, 80 */
-# "#285cf7",  /*  4: blue     225,  100, 80 */
-# "#E62EA9",  /*  5: magenta  320,  100, 80 */
-# "#2EE6E6",  /*  6: cyan     180,  100, 80 */
-# "#FFFFFF",  /*  7: white     45,  25,  90 */
+# Standard colors
+# "#2e1a1a1a",  /*  0: black     */
+# "#cc0000",  /*  1: red       */
+# "#4e9a06",  /*  2: green     */
+# "#c4a000",  /*  3: yellow    */
+# "#3465a4",  /*  4: blue      */
+# "#75507b",  /*  5: magenta   */
+# "#06989a",  /*  6: cyan      */
+# "#d3d7cf",  /*  7: white     */
+
+# Brighter colors
+# "#555753",  /*  8: bright black   */
+# "#ef2929",  /*  9: bright red     */
+# "#8ae234",  /* 10: bright green   */
+# "#fce94f",  /* 11: bright yellow  */
+# "#729fcf",  /* 12: bright blue    */
+# "#ad7fa8",  /* 13: bright magenta */
+# "#34e2e2",  /* 14: bright cyan    */
+# "#eeeeec",  /* 15: bright white   */
 
 if test "$TERM" = "linux"
-    printf '\033]P000060f'  # black
-    printf '\033]P1991f1f'  # red
-    printf '\033]P25c991f'  # green
-    printf '\033]P3997b1f'  # yellow
-    printf '\033]P42e53bf'  # blue
-    printf '\033]P5991f70'  # magenta
-    printf '\033]P61f9999'  # cyan
-    printf '\033]P7e6e6e6'  # white
-    printf '\033]P8333026'  # brighter black
-    printf '\033]P9E62E2E'  # brighter red
-    printf '\033]PA8AE62E'  # brighter green
-    printf '\033]PBE6B82E'  # brighter yellow
-    printf '\033]PC285cf7'  # brighter blue
-    printf '\033]PDE62EA9'  # brighter magenta
-    printf '\033]PE2EE6E6'  # brighter cyan
-    printf '\033]PFFFFFFF'  # brighter white
-    clear  # for background artifacting
+    printf '\033]P01a1a1a'  # black
+    printf '\033]P1cc0000'  # red
+    printf '\033]P24e9a06'  # green
+    printf '\033]P3c4a000'  # yellow
+    printf '\033]P43465a4'  # blue
+    printf '\033]P575507b'  # magenta
+    printf '\033]P606989a'  # cyan
+    printf '\033]P7d3d7cf'  # white
+    printf '\033]P8555753'  # bright black
+    printf '\033]P9ef2929'  # bright red
+    printf '\033]PA8ae234'  # bright green
+    printf '\033]PBfce94f'  # bright yellow
+    printf '\033]PC729fcf'  # bright blue
+    printf '\033]PDad7fa8'  # bright magenta
+    printf '\033]PE34e2e2'  # bright cyan
+    printf '\033]PFeeeeec'  # bright white
+    clear
 end
 
 
