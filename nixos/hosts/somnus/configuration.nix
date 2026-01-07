627ab92cf62b26c703c0423f18dd233c683bca8e
silvus <silvus@silvus.fr>
2026-01-08 00:23:59 +0100

    Fix Somnus resolution

--- nixos/hosts/somnus/configuration.nix
+++ nixos/hosts/somnus/configuration.nix
@@ -8,11 +8,19 @@
     ../../modules/base.nix
     ../../modules/keyboard.nix
     ../../modules/desktop_base.nix
-    ../../modules/desktop_sway.nix
+    # ../../modules/desktop_sway.nix
+    ../../modules/desktop_awesome.nix
     # ../../modules/gaming.nix
     # ../../modules/security.nix
     ../../modules/sandman.nix
     ../../modules/development.nix
   ];
+
+  services.xserver = {
+    enable = true;
+    resolutions = [
+      {x = 1920; y = 1080;}
+    ];
+  };
 
 }
