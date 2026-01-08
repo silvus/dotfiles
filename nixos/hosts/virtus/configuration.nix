100a663f466169f80a7b0caa1d3ee2d142683277
silvus <silvus@silvus.fr>
2026-01-08 10:55:32 +0100

    Prepare virtus dual boot

--- nixos/hosts/virtus/configuration.nix
+++ nixos/hosts/virtus/configuration.nix
@@ -17,4 +17,30 @@
     ../../modules/development.nix
     ../../modules/development_containers.nix
   ];
+
+  # Bootloader (dual boot with Debian)
+  # https://wiki.nixos.org/wiki/Systemd/boot
+  boot.loader = {
+    systemd-boot = {
+      # The sort key used for Nix entries
+      # sortKey = "a_01";
+
+      # Dual boot with Debian
+      # Use "d" key to change the default entry (the arrow)!
+      extraEntries."debian.conf" = ''
+        title Debian
+        efi   /efi/debian/grubx64.efi
+      '';
+        # sort-key z_99_debian
+    };
+  };
+
+  # Nvidia
+  # https://wiki.nixos.org/wiki/NVIDIA
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;  # Open-source kernel modules are preferred
+
+  environment.systemPackages = with pkgs; [
+    keymapp
+  ];
 }
