6569fff832a7849b7b2bba9963d99b443fa3b12f
silvus <silvus@silvus.fr>
2026-01-08 10:53:38 +0100

    Add swap to somnus

--- nixos/hosts/somnus/hardware-configuration.nix
+++ nixos/hosts/somnus/hardware-configuration.nix
@@ -24,7 +24,10 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 4*1024; # 4GB
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
