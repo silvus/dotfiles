658cfc03202734e67b875a7469601afb35554670
silvus <silvus@silvus.fr>
2026-02-13 12:35:10 +0100

    Add ipv6 DNS

--- nixos/modules/base.nix
+++ nixos/modules/base.nix
@@ -34,7 +34,10 @@
   networking.networkmanager.enable = true;
 
   # Set DNS
-  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.nameservers = [
+    "1.1.1.1" "2606:4700:4700::1111"
+    "8.8.8.8" "2001:4860:4860::8888"
+  ];
 
   # Set your time zone
   time.timeZone = "Europe/Paris";
