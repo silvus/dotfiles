39b6eb74d8132327556381d41688e4322ae974cf
silvus <silvus@silvus.fr>
2026-05-17 12:54:09 +0200

    Try to be Termux compatible

--- app/.config/bash/90_ascii.sh
+++ app/.config/bash/90_ascii.sh
@@ -1,35 +1,76 @@
-
-# Print ascii art and some system infos
+# Print ascii art and system information (portable: Linux + Termux)
 # --------------------------------------------------------------------
 __asciiart() {
-	local OS=$(uname -s)
-	local ARCH=$(uname -m)
-	local KERNEL=$(uname -r)
-	local HOST=$(hostname -s)
-	local IP_LOCAL=$(ip -4 -o addr show scope global | awk '{print $4}' | cut -d/ -f1 | head -n1)
-	local SHELL=$(ps -p $$ | awk '$1 != "PID" {print $(NF)}' | tr -d '()')
-	local UPTIME=$(awk '{printf "%d hours, %d minutes\n", $1/3600, ($1%3600)/60}' /proc/uptime)
-	local DISKSPACE=$(df --output=pcent / | sed -nr '/[[:digit:]]/{s/[[:space:]]+([[:digit:]]+)%/\1/;p}')
 
-	# Colors
-	local TXTRED=$(tput setaf 1)
-	local TXTGREEN=$(tput setaf 2)
-	local TXTBLUE=$(tput setaf 4)
-	local TXTYELLOW=$(tput setaf 3)
-	local TXTBLUE=$(tput setaf 4)
-	local TXTPURPLE=$(tput setaf 5)
-	local TXTCYAN=$(tput setaf 6)
-	local TXTRESET=$(tput sgr0)
+    # System info
+    local OS
+    OS=$(uname -s 2>/dev/null)
 
-	echo -e "$TXTBLUE"'      .--.      '"${TXTCYAN}${USER}${TXTRESET}@${TXTYELLOW}${HOST}${TXTRESET}"
-	echo -e "$TXTBLUE"'     |o_o |     '"${TXTCYAN}IP      ${TXTGREEN}$IP_LOCAL${TXTRESET}"
-	echo -e "$TXTBLUE"'     |:_/ |     '"${TXTCYAN}Kernel  ${TXTGREEN}$ARCH $KERNEL${TXTRESET}"
-	echo -e "$TXTBLUE"'    //   \ \    '"${TXTCYAN}Shell   ${TXTGREEN}$SHELL${TXTRESET}"
-	echo -e "$TXTBLUE"'   (|     | )   '"${TXTCYAN}Disk /  ${TXTGREEN}$DISKSPACE%${TXTRESET}"
-	echo -e "$TXTBLUE"'  / \_   _/ \   '"${TXTRESET}"
-	echo -e "$TXTBLUE"'  \___)=(___/   '"${TXTCYAN}Uptime  ${TXTGREEN}$UPTIME${TXTRESET}"
-	echo ''
+    local ARCH
+    ARCH=$(uname -m 2>/dev/null)
+
+    local KERNEL
+    KERNEL=$(uname -r 2>/dev/null)
+
+    local HOST
+    HOST=$(hostname 2>/dev/null)
+
+    # IP address (Termux-safe fallback chain)
+    local IP_LOCAL="N/A"
+    if command -v hostname >/dev/null 2>&1; then
+        IP_LOCAL=$(hostname -I 2>/dev/null | awk '{print $1}')
+    fi
+    if [ -z "$IP_LOCAL" ]; then
+        if command -v ifconfig >/dev/null 2>&1; then
+            IP_LOCAL=$(ifconfig 2>/dev/null |
+                awk '/inet / && $2 != "127.0.0.1" {print $2; exit}')
+        fi
+    fi
+    if [ -z "$IP_LOCAL" ] && command -v getprop >/dev/null 2>&1; then
+        IP_LOCAL=$(getprop dhcp.wlan0.ipaddress 2>/dev/null)
+    fi
+    [ "$IP_LOCAL" ] || IP_LOCAL="N/A"
+
+    # Shell detection
+    local SHELL
+    SHELL=$(ps -p $$ 2>/dev/null | awk 'NR==2 {print $NF}')
+
+    # Uptime (Termux may restrict /proc/uptime)
+    local UPTIME="Unavailable"
+    if [ -r /proc/uptime ]; then
+        UPTIME=$(awk '{
+            printf "%d hours, %d minutes",
+            $1/3600,
+            ($1%3600)/60
+        }' /proc/uptime)
+    fi
+
+    # Disk usage (no GNU df --output support on Termux)
+    local DISKSPACE
+    DISKSPACE=$(df / 2>/dev/null | awk 'NR==2 {gsub("%","",$5); print $5}')
+
+    [ "$DISKSPACE" ] || DISKSPACE="N/A"
+
+    # Colors
+    local TXTGREEN TXTBLUE TXTYELLOW TXTCYAN TXTRESET
+
+    # TXTRED=$(tput setaf 1)
+    TXTGREEN=$(tput setaf 2)
+    TXTYELLOW=$(tput setaf 3)
+    TXTBLUE=$(tput setaf 4)
+    # TXTPURPLE=$(tput setaf 5)
+    TXTCYAN=$(tput setaf 6)
+    TXTRESET=$(tput sgr0)
+
+    # Output
+    echo -e "${TXTBLUE}      .--.      ${TXTRESET}${TXTCYAN}${USER}${TXTRESET}@${TXTYELLOW}${HOST}${TXTRESET}"
+    echo -e "${TXTBLUE}     |o_o |     ${TXTRESET}${TXTCYAN}IP      ${TXTGREEN}${IP_LOCAL}${TXTRESET}"
+    echo -e "${TXTBLUE}     |:_/ |     ${TXTRESET}${TXTCYAN}Kernel  ${TXTGREEN}${OS} ${ARCH} ${KERNEL}${TXTRESET}"
+    echo -e "${TXTBLUE}    //   \ \    ${TXTRESET}${TXTCYAN}Shell   ${TXTGREEN}${SHELL}${TXTRESET}"
+    echo -e "${TXTBLUE}   (|     | )   ${TXTRESET}${TXTCYAN}Disk /  ${TXTGREEN}${DISKSPACE}%${TXTRESET}"
+    echo -e "${TXTBLUE}  / \_   _/ \   ${TXTRESET}"
+    echo -e "${TXTBLUE}  \___)=(___/   ${TXTRESET}${TXTCYAN}Uptime  ${TXTGREEN}${UPTIME}${TXTRESET}"
+    echo ''
 }
 
 __asciiart
-
--- app/.config/fish/functions/fish_greeting.fish
+++ app/.config/fish/functions/fish_greeting.fish
@@ -1,27 +1,100 @@
 function fish_logo \
     --description="Fish-shell colorful ASCII-art logo" \
     --argument-names outer_color medium_color inner_color mouth eye
-    # https://github.com/laughedelic/fish_logo
 
-    # defaults:
-    [ $outer_color  ]; or set outer_color  'blue'
-    [ $medium_color ]; or set medium_color 'cyan'
-    [ $inner_color  ]; or set inner_color  'green'
+    # Default colours and symbols
+    [ $outer_color ]; or set outer_color blue
+    [ $medium_color ]; or set medium_color cyan
+    [ $inner_color ]; or set inner_color green
     [ $mouth ]; or set mouth '['
-    [ $eye   ]; or set eye   'O'
+    [ $eye ]; or set eye O
 
-    # shortcuts:
+    # Colour shortcuts
     set o (set_color $outer_color)
     set m (set_color $medium_color)
     set i (set_color $inner_color)
 
+    # Hostname
+    # hostname -s is not portable on Termux.
+    set host_name (hostname 2>/dev/null)
+    [ "$host_name" ]; or set host_name unknown
+
+    # IP address detection
+    # Termux restricts many iproute2/netlink operations.
+    # Use multiple fallbacks.
+    set ip_addr ""
+
+    # First attempt: hostname -I
+    if command -q hostname
+        set ip_addr (
+            hostname -I 2>/dev/null | awk '{print $1}'
+        )
+    end
+
+    # Second attempt: ifconfig (requires net-tools package)
+    if test -z "$ip_addr"
+        if command -q ifconfig
+            set ip_addr (
+                ifconfig 2>/dev/null |
+                awk '/inet / && $2 != "127.0.0.1" {print $2; exit}'
+            )
+        end
+    end
+
+    # Third attempt: Android system property
+    if test -z "$ip_addr"
+        if command -q getprop
+            set ip_addr (
+                getprop dhcp.wlan0.ipaddress 2>/dev/null
+            )
+        end
+    end
+
+    # Final fallback
+    [ "$ip_addr" ]; or set ip_addr N/A
+
+    # Uptime
+    # Termux devices deny access to /proc/uptime.
+    if test -r /proc/uptime
+        set uptime_text (
+            awk '{
+                printf "%d hours, %d minutes",
+                $1/3600,
+                ($1%3600)/60
+            }' /proc/uptime
+        )
+    else
+        set uptime_text Unavailable
+    end
+
+    # Disk usage
+    # GNU df --output=pcent is unavailable on Toybox/BusyBox.
+    set disk_percent (
+        df / 2>/dev/null |
+        awk 'NR==2 {gsub("%","",$5); print $5}'
+    )
+    [ "$disk_percent" ]; or set disk_percent N/A
+
+    # Current date
+    # Avoid GNU-specific sed formatting.
+    set current_date (
+        date 2>/dev/null |
+        awk '{
+            for (i = 1; i <= NF; i++) {
+                $i = toupper(substr($i,1,1)) substr($i,2)
+            }
+            print
+        }'
+    )
+
+    # Render ASCII logo
     echo '                 '$o'___
-  ___======____='$m'-'$i'-'$m'-='$o')                     '$o$USER'@'(hostname -s)$o'
-/T            \_'$i'--='$m'=='$o')            '$m'IP      '$i(ip -4 -o addr show scope global | awk '{print $4}' | cut -d/ -f1 | head -n1)$o'
-'$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')            '$m'Uptime  '$i(awk '{printf "%d hours, %d minutes\n", $1/3600, ($1%3600)/60}' /proc/uptime)$o'
+  ___======____='$m'-'$i'-'$m'-='$o')                     '$o$USER'@'$host_name$o'
+/T            \_'$i'--='$m'=='$o')            '$m'IP      '$i$ip_addr$o'
+'$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')            '$m'Uptime  '$i$uptime_text$o'
  \      / )J'$m'~~    '$o'\\'$i'-='$o')            '$m'Kernel  '$i(uname -s) (uname -m) (uname -r)$o'
-  \\\\___/  )JJ'$m'~'$i'~~   '$o'\)             '$m'Disk /  '$i(df --output=pcent  / | sed -nr '/[[:digit:]]/{s/[[:space:]]+([[:digit:]]+)%/\1/;p}')'%'$o'
-   \_____/JJJ'$m'~~'$i'~~    '$o'\\            '$m'Date    '$i(date | sed 's/[^ _-]*/\u&/g')$o'
+  \\\\___/  )JJ'$m'~'$i'~~   '$o'\)             '$m'Disk /  '$i$disk_percent'%'$o'
+   \_____/JJJ'$m'~~'$i'~~    '$o'\\            '$m'Date    '$i$current_date$o'
    '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\
   (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_
   '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__
@@ -32,7 +105,7 @@ function fish_logo \
                        (UU)'(set_color normal)
 end
 
+# Fish greeting hook
 function fish_greeting
-	fish_logo
+    fish_logo
 end
-
